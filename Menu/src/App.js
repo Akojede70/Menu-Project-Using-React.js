089b96f195e0b642b8b6a95b6d9fab3d0a203862
Akojede70 <88320958+Akojede70@users.noreply.github.com>
2023-03-10 11:04:39 +0100

    Update App.js

--- Menu/src/App.js
+++ Menu/src/App.js
@@ -1,19 +1,24 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
-const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+// the all is for the ALL BUTTON, the "items" refer to all the array in the data file
+// creates a new Set object from the array of category, which is the different button. Breakfast, Lunch, Shakes, Dinner.
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState(allCategories);
 
   const filterItems = (category) => {
-    if (category === 'all') {
+    if (category === "all") {
+      // if category in the array is equall to all set all the itmes in the data file in the ALL button
       setMenuItems(items);
       return;
     }
     const newItems = items.filter((item) => item.category === category);
+    // each item in each category i.e. breakfast, lunch and dinner. e.t.c.
     setMenuItems(newItems);
   };
 
